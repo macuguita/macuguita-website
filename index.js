1a7bb70578c2ddb9a3865dfd076d505dc6e7005c
macuguita <raulpripri@gmail.com>
2025-03-07 19:41:22 +0100

    hopefully fixed phone stuff

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 /* Toggle the side navigation */
 function openNav() {
     const sidenav = document.getElementById("mySidenav");
-    const main = document.getElementById("main");
-
-    if (sidenav.style.width === "250px") {
-        // If the side navigation is open, close it
-        sidenav.style.width = "0";
-        main.style.marginLeft = "0";
-    } else {
-        // If the side navigation is closed, open it
-        sidenav.style.width = "250px";
-        main.style.marginLeft = "250px";
-    }
+    sidenav.classList.toggle("open"); // Toggle the "open" class
 }
