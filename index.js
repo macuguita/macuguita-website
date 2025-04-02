2fa704460dab337e1b0525e031f2afc18c543b55
macuguita <raulpripri@gmail.com>
2025-04-02 11:10:57 +0200

    Removed js, no fake languajes allowed!!

--- index.js
+++
@@ -1,5 +0,0 @@
-/* Toggle the side navigation */
-function openNav() {
-    const sidenav = document.getElementById("mySidenav");
-    sidenav.classList.toggle("open"); // Toggle the "open" class
-}
